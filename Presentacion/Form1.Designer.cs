a8c7300b22921096a67d6126f2a9ae1df37fe248
Martín Quiroga <quiroga.martin.augusto@gmail.com>
2023-01-21 10:50:02 -0300

    varios cambios

--- Presentacion/Form1.Designer.cs
+++ Presentacion/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Presentacion
 {
-    partial class Form1
+    partial class formPresentacion
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,106 +28,134 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.gridMedicos = new System.Windows.Forms.DataGridView();
-            this.gridPacientes = new System.Windows.Forms.DataGridView();
-            this.clinicos = new System.Windows.Forms.ListBox();
-            this.habitaciones = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.gridMedicos = new System.Windows.Forms.DataGridView();
             this.label2 = new System.Windows.Forms.Label();
+            this.gridPacientes = new System.Windows.Forms.DataGridView();
             this.label3 = new System.Windows.Forms.Label();
+            this.listClinicos = new System.Windows.Forms.ListBox();
+            this.listHabitaciones = new System.Windows.Forms.ListBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.btnRegMed = new System.Windows.Forms.Button();
+            this.btnRegPacnte = new System.Windows.Forms.Button();
+            this.btnDatHabitacion = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.gridMedicos)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridPacientes)).BeginInit();
             this.SuspendLayout();
             // 
-            // gridMedicos
-            // 
-            this.gridMedicos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridMedicos.Location = new System.Drawing.Point(12, 41);
-            this.gridMedicos.Name = "gridMedicos";
-            this.gridMedicos.Size = new System.Drawing.Size(416, 150);
-            this.gridMedicos.TabIndex = 0;
-            this.gridMedicos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridMedicos_CellContentClick);
-            // 
-            // gridPacientes
-            // 
-            this.gridPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridPacientes.Location = new System.Drawing.Point(12, 238);
-            this.gridPacientes.Name = "gridPacientes";
-            this.gridPacientes.Size = new System.Drawing.Size(313, 150);
-            this.gridPacientes.TabIndex = 1;
-            this.gridPacientes.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gridPacientes_CellContentClick);
-            // 
-            // clinicos
-            // 
-            this.clinicos.FormattingEnabled = true;
-            this.clinicos.Location = new System.Drawing.Point(461, 41);
-            this.clinicos.Name = "clinicos";
-            this.clinicos.Size = new System.Drawing.Size(178, 134);
-            this.clinicos.TabIndex = 2;
-            this.clinicos.SelectedIndexChanged += new System.EventHandler(this.clinicos_SelectedIndexChanged);
-            // 
-            // habitaciones
-            // 
-            this.habitaciones.FormattingEnabled = true;
-            this.habitaciones.Location = new System.Drawing.Point(352, 246);
-            this.habitaciones.Name = "habitaciones";
-            this.habitaciones.Size = new System.Drawing.Size(287, 134);
-            this.habitaciones.TabIndex = 3;
-            this.habitaciones.SelectedIndexChanged += new System.EventHandler(this.habitaciones_SelectedIndexChanged);
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 12);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Médicos de la clínica: ";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Médicos de la clínica:";
+            // 
+            // gridMedicos
+            // 
+            this.gridMedicos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridMedicos.Location = new System.Drawing.Point(15, 25);
+            this.gridMedicos.Name = "gridMedicos";
+            this.gridMedicos.Size = new System.Drawing.Size(310, 145);
+            this.gridMedicos.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 208);
+            this.label2.Location = new System.Drawing.Point(12, 211);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 13);
-            this.label2.TabIndex = 5;
+            this.label2.TabIndex = 2;
             this.label2.Text = "Pacientes:";
+            // 
+            // gridPacientes
+            // 
+            this.gridPacientes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridPacientes.Location = new System.Drawing.Point(15, 227);
+            this.gridPacientes.Name = "gridPacientes";
+            this.gridPacientes.Size = new System.Drawing.Size(310, 139);
+            this.gridPacientes.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(349, 208);
+            this.label3.Location = new System.Drawing.Point(361, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(72, 13);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Habitaciones:";
+            this.label3.Size = new System.Drawing.Size(90, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Médicos clínicos:";
+            // 
+            // listClinicos
+            // 
+            this.listClinicos.FormattingEnabled = true;
+            this.listClinicos.Location = new System.Drawing.Point(364, 25);
+            this.listClinicos.Name = "listClinicos";
+            this.listClinicos.Size = new System.Drawing.Size(256, 147);
+            this.listClinicos.TabIndex = 5;
+            // 
+            // listHabitaciones
+            // 
+            this.listHabitaciones.FormattingEnabled = true;
+            this.listHabitaciones.Location = new System.Drawing.Point(364, 219);
+            this.listHabitaciones.Name = "listHabitaciones";
+            this.listHabitaciones.Size = new System.Drawing.Size(256, 147);
+            this.listHabitaciones.TabIndex = 6;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(458, 12);
+            this.label4.Location = new System.Drawing.Point(361, 203);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 13);
+            this.label4.Size = new System.Drawing.Size(72, 13);
             this.label4.TabIndex = 7;
-            this.label4.Text = "Médicos clínicos:";
+            this.label4.Text = "Habitaciones:";
             // 
-            // Form1
+            // btnRegMed
+            // 
+            this.btnRegMed.Location = new System.Drawing.Point(15, 176);
+            this.btnRegMed.Name = "btnRegMed";
+            this.btnRegMed.Size = new System.Drawing.Size(101, 23);
+            this.btnRegMed.TabIndex = 8;
+            this.btnRegMed.Text = "Registrar médico";
+            this.btnRegMed.UseVisualStyleBackColor = true;
+            // 
+            // btnRegPacnte
+            // 
+            this.btnRegPacnte.Location = new System.Drawing.Point(15, 376);
+            this.btnRegPacnte.Name = "btnRegPacnte";
+            this.btnRegPacnte.Size = new System.Drawing.Size(101, 23);
+            this.btnRegPacnte.TabIndex = 9;
+            this.btnRegPacnte.Text = "Registrar paciente";
+            this.btnRegPacnte.UseVisualStyleBackColor = true;
+            // 
+            // btnDatHabitacion
+            // 
+            this.btnDatHabitacion.Location = new System.Drawing.Point(364, 376);
+            this.btnDatHabitacion.Name = "btnDatHabitacion";
+            this.btnDatHabitacion.Size = new System.Drawing.Size(113, 23);
+            this.btnDatHabitacion.TabIndex = 11;
+            this.btnDatHabitacion.Text = "Datos habitaciones";
+            this.btnDatHabitacion.UseVisualStyleBackColor = true;
+            // 
+            // formPresentacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(651, 401);
+            this.ClientSize = new System.Drawing.Size(632, 411);
+            this.Controls.Add(this.btnDatHabitacion);
+            this.Controls.Add(this.btnRegPacnte);
+            this.Controls.Add(this.btnRegMed);
             this.Controls.Add(this.label4);
+            this.Controls.Add(this.listHabitaciones);
+            this.Controls.Add(this.listClinicos);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.habitaciones);
-            this.Controls.Add(this.clinicos);
             this.Controls.Add(this.gridPacientes);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.gridMedicos);
-            this.Name = "Form1";
+            this.Controls.Add(this.label1);
+            this.Name = "formPresentacion";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridMedicos)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridPacientes)).EndInit();
             this.ResumeLayout(false);
@@ -137,14 +165,17 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView gridMedicos;
-        private System.Windows.Forms.DataGridView gridPacientes;
-        private System.Windows.Forms.ListBox clinicos;
-        private System.Windows.Forms.ListBox habitaciones;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView gridMedicos;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView gridPacientes;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listClinicos;
+        private System.Windows.Forms.ListBox listHabitaciones;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRegMed;
+        private System.Windows.Forms.Button btnRegPacnte;
+        private System.Windows.Forms.Button btnDatHabitacion;
     }
 }
 
--- Presentacion/Form1.cs
+++ Presentacion/Form1.cs
@@ -12,55 +12,13 @@ using Negocio;
 
 namespace Presentacion
 {
-    public partial class Form1 : Form
+    public partial class formPresentacion : Form
     {
-        public Form1()
+        public formPresentacion()
         {
             InitializeComponent();
         }
 
-        private void gridMedicos_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void gridPacientes_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            gridMedicos.DataSource = AbmMedico.Listar();
-
-            gridPacientes.DataSource = AbmPaciente.Listar();
-
-            foreach (Medico medico in AbmMedico.Listar("Clínico"))
-            {
-                clinicos.Items.Add("Nombre: " + medico.Nombre + " " + medico.Apellido);
-            }
-
-            foreach (Habitacion habitacion in AbmHabitacion.Listar())
-            {
-                string estadoHabitacion;
-
-                if (habitacion.Estado) estadoHabitacion = "Ocupada";
-                else estadoHabitacion = "Libre";
-
-                habitaciones.Items.Add("Número: " + habitacion.Numero + "  Estado: " + estadoHabitacion);
-            }
-            
-
-        }
-
-        private void clinicos_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void habitaciones_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-        }
+        
     }
 }
