04efb76e5bd8a13c8676b317950b30b3e967309a
Martín Quiroga <quiroga.martin.augusto@gmail.com>
2023-01-21 11:13:55 -0300

    .

---
+++ Datos/Dac/AbmHabitacion.cs
@@ -0,0 +1,42 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public static class AbmHabitacion
+    {
+        public static List<Habitacion> Listar()
+        {
+            //TODO...
+            return null;
+        }
+
+        public static List<Habitacion> Listar(bool estado)
+        {
+            //TODO...
+            return null;
+        }
+
+        public static int Insertar(Habitacion habitacion)
+        {
+            //TODO...
+            return 0;
+        }
+
+        public static int Eliminar(int id)
+        {
+            //TODO...
+            return 0;
+        }
+
+        public static Habitacion TraerUno(int numero)
+        {
+            //TODO...
+            return null;
+        }
+    }
+}
---
+++ Entidades/Enfermero.cs
@@ -0,0 +1,32 @@
+﻿using Entidades.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    [Table("Enfermeros")]
+    public class Enfermero : Persona
+    {
+
+        public Enfermero(string nombre, string apellido, string domicilio, string telefono, string email, int enfermeroId, string cuil):base(nombre, apellido, domicilio, telefono, email)
+        {
+            EnfermeroId = enfermeroId;
+            Cuil = cuil;
+        }
+
+        public int EnfermeroId { get; set; }
+
+        [Column(TypeName = "varchar")]
+        [StringLength(50)]
+        [Required]
+        public string Cuil { get; set; }
+
+        public Clinica Clinica { get; set; }
+
+    }
+}
